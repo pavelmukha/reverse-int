6f4e9e9d2b359d570a29339c675cef8804919762
pavelmukha <pavel445131320@gmail.com>
2025-07-26 14:59:20 +0300

    test done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  let m = String(Math.abs(n));
+  m = m.split('');
+  m = m.reverse();
+  m = m.join('');
+  return m;
 };
